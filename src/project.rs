4737a4c0a773a67f71ed64efbfe6631f0cf9b335
Thib <thibaultm@matrix.org>
2022-09-05 11:55:10 +0200

    Make fmt happy

--- src/project.rs
+++ src/project.rs
@@ -12,7 +12,6 @@ pub struct Project {
 
 impl Project {
     pub fn html_details(&self) -> String {
-
         format!(
             "<b>Project Details</b><br>\
             <b>Emoji</b>: {} <br>\
@@ -20,12 +19,7 @@ impl Project {
             <b>Description</b>: {} <br>\
             <b>Website</b>: {} <br>\
             <b>Default Section</b>: {} <br>",
-            self.emoji,
-            self.title,
-            self.name,
-            self.description,
-            self.website,
-            self.default_section,
+            self.emoji, self.title, self.name, self.description, self.website, self.default_section,
         )
     }
 }
